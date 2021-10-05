9420b62318b111483f0b7f57982782c98f0e38d4
ramondorosario <ramondorosario@gmail.com>
2021-10-04 21:50:54 -0300

    style: responsivity in Logo component

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,8 +1,10 @@
 import styled, { css, FlattenSimpleInterpolation } from 'styled-components';
-
+import media from 'styled-media-query';
 import { LogoProps, Size } from '.';
 
-const wrapperModifiers: Record<Size, () => FlattenSimpleInterpolation> = {
+const wrapperModifiers: Record<Size, () => FlattenSimpleInterpolation> & {
+	hideOnMobile(): FlattenSimpleInterpolation;
+} = {
 	large: () => css`
 		width: 20rem;
 		height: 6rem;
@@ -11,12 +13,28 @@ const wrapperModifiers: Record<Size, () => FlattenSimpleInterpolation> = {
 		width: 11rem;
 		height: 3.3rem;
 	`,
+	hideOnMobile: () => css`
+		${media.lessThan('medium')`
+		 width: 5.8rem;
+		 height: 4.5rem;
+
+		 svg {
+			 height: 4.5rem;
+			 pointer-events: none;
+		 }
+
+		 .text {
+			 display: none;
+		 }
+	 `}
+	`,
 };
 
 export const Wrapper = styled.div<LogoProps>`
-	${({ theme, color, size }) => css`
+	${({ theme, color, size, hideOnMobile }) => css`
 		color: ${theme.colors[color!]};
 
 		${!!size && wrapperModifiers[size]};
+		${!!hideOnMobile && wrapperModifiers.hideOnMobile}
 	`}
 `;
