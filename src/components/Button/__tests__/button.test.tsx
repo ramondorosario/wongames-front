08a836a5f54e02be90fb947f2a04344cb42e010a
ramondorosario <ramondorosario@gmail.com>
2021-10-06 10:51:40 -0300

    test: Button component

---
+++ src/components/Button/__tests__/button.test.tsx
@@ -0,0 +1,53 @@
+import { AddShoppingCart } from '@styled-icons/material-outlined';
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helpers';
+import { Button } from '..';
+
+describe('<Button />', () => {
+	it('Should render a medium button by default', () => {
+		renderWithTheme(<Button>Medium</Button>);
+
+		expect(screen.getByRole('button', { name: 'Medium' })).toHaveStyle({
+			padding: '0 3.2rem',
+			height: '4rem',
+			'font-size': '1.4rem',
+		});
+	});
+
+	it('Should render a small button', () => {
+		renderWithTheme(<Button size="small">Small</Button>);
+
+		expect(screen.getByRole('button', { name: 'Small' })).toHaveStyle({
+			padding: '0 3.2rem',
+			height: '3rem',
+			'font-size': '1.2rem',
+		});
+	});
+
+	it('Should render a large button', () => {
+		renderWithTheme(<Button size="large">Large</Button>);
+
+		expect(screen.getByRole('button', { name: 'Large' })).toHaveStyle({
+			padding: '0 4.8rem',
+			height: '5rem',
+			'font-size': '1.6rem',
+		});
+	});
+
+	it('Should render a full width button', () => {
+		renderWithTheme(<Button fullWidth>Full Width</Button>);
+
+		expect(screen.getByRole('button', { name: 'Full Width' })).toHaveStyle({
+			width: '100%',
+		});
+	});
+
+	it('Should render a button with icon', () => {
+		renderWithTheme(
+			<Button icon={<AddShoppingCart data-testid="icon" />}>With Icon</Button>
+		);
+
+		expect(screen.getByTestId('icon')).toBeInTheDocument();
+		expect(screen.getByText('With Icon')).toBeInTheDocument();
+	});
+});
