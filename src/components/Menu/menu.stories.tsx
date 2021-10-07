d08848cff92c597665c4b610611c3ba4591aac68
ramondorosario <ramondorosario@gmail.com>
2021-10-06 23:50:04 -0300

    feat: create menu stories

---
+++ src/components/Menu/menu.stories.tsx
@@ -0,0 +1,23 @@
+import { Story, Meta } from '@storybook/react';
+import { Menu, MenuProps } from '.';
+
+export default {
+	title: 'Menu',
+	compoentn: Menu,
+} as Meta;
+
+export const Default: Story<MenuProps> = (args) => <Menu {...args} />;
+
+Default.args = {
+	username: '',
+};
+
+Default.parameters = {
+	viewport: {
+		defaultViewport: 'mobile1',
+	},
+	backgrounds: {
+		default: 'dark',
+	},
+	layout: 'fullscreen',
+};
